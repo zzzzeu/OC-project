4a854cf6b1538b8994bfe55c967245deb6fc8ed3
zzzzeu <zouanei@outlook.com>
2020-11-26 20:05:49 +0800

    add uiview

--- OC project/OC project/ViewController.m
+++ OC project/OC project/ViewController.m
@@ -8,6 +8,37 @@
 
 #import "ViewController.h"
 
+@interface TestView: UIView
+@end
+
+@implementation TestView
+
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview{
+    [super willMoveToSuperview:newSuperview];
+}
+
+-(void)didMoveToSuperview{
+    [super didMoveToSuperview];
+}
+
+-(void)willMoveToWindow:(nullable UIWindow *)newWindow{
+    [super willMoveToWindow:newWindow];
+}
+
+-(void)didMoveToWindow{
+    [super didMoveToWindow];
+}
+
+@end
+
 @interface ViewController ()
 
 @end
@@ -16,8 +47,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    TestView *view2 = [[TestView alloc] init];
+    view2.backgroundColor = [UIColor greenColor];
+    view2.frame = CGRectMake(150, 150, 100, 100);
+    [self.view addSubview:view2];
+    
 }
-
 
 @end
